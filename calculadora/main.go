a41eb121022e30731c1f0705ea10009785a95363
Apolinario <whoami.apolo@gmail.com>
2024-10-23 23:58:11 -0300

    feat: add conteudo complementar sobre go

--- calculadora/main.go
+++ calculadora/main.go
@@ -1,1 +1,21 @@
-package calculadora
+package main
+
+import (
+	"calculator/math"
+	"fmt"
+)
+
+func main() {
+	sum, sub, multi, div := math.Calc(5, 5)
+	fmt.Println("Sum: ", sum, "\nSub: ", sub, "\nMulti: ", multi, "\nDiv: ", div)
+
+	//e := echo.New()
+}
+
+/*
+add pacotes internos
+go mod init calculator
+
+// add pacote externo
+go mod tidy
+*/
--- calculadora/math/math.go
+++ calculadora/math/math.go
@@ -1,1 +1,22 @@
 package math
+
+func Calc(a, b int) (int, int, int, int) {
+	outSum := sum(a, b)
+	outSub := sub(a, b)
+	outMulti := mult(a, b)
+	outDiv := div(a, b)
+
+	return outSum, outSub, outMulti, outDiv
+}
+func sum(a, b int) int {
+	return a + b
+}
+func sub(a, b int) int {
+	return a - b
+}
+func mult(a, b int) int {
+	return a * b
+}
+func div(a, b int) int {
+	return a / b
+}
